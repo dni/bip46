cf81ea7bb5bc5cbb4208617d87831e13735b501c
dni ⚡ <office@dnilabs.com>
2024-09-23 08:52:47 +0200

    feat: add example for creating a timelock

--- examples/example_create_timelock.py
+++ examples/example_create_timelock.py
@@ -1,10 +1,16 @@
-# BIP46 - Address Scheme for Timelocked Fidelity Bonds
+from datetime import datetime
 
-this library is alpha and a work in progress
+from bip46 import (
+    create_lockscript,
+    hdkey_derive,
+    hdkey_from_mnemonic,
+    hdkey_to_pubkey,
+    hdkey_to_wif,
+    lockdate_to_derivation_path,
+    lockscript_address,
+    lockscript_pubkey,
+)
 
-## example
-### create a lockscript
-```python
 lock_date = datetime(2042, 6, 1)
 lock_path = lockdate_to_derivation_path(lock_date)
 
@@ -18,4 +24,11 @@ lock_pub_key = hdkey_to_pubkey(lock_key)
 lock_script = create_lockscript(lock_date, lock_pub_key)
 script_pubkey = lockscript_pubkey(lock_script)
 script_address = lockscript_address(script_pubkey, network="main")
-```
+
+print(f"lock date: {lock_date}")
+print(f"lock path: {lock_path}")
+print(f"lock key: {lock_priv_key}")
+print(f"lock pubkey: {lock_pub_key.hex()}")
+print(f"lock script: {lock_script.hex()}")
+print(f"script pubkey: {script_pubkey.hex()}")
+print(f"script address: {script_address}")
